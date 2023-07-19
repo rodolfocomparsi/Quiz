6fe476ca1ecd15632b25123bee4832b1db044e0b
rodolfocomparsi <rodolfocomparsi@hotmail.com>
2023-07-19 17:58:53 -0300

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Rodolfo Comparsi on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
